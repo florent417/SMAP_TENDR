4f0c4dfad8447833835107b013d0881b0f4228d1
Berthelmaster <berthel0802@gmail.com>
2020-04-16 19:43:19 +0200

    extended services & added services to manifest

--- TENdr/app/src/main/java/smap/gr15/appproject/tendr/services/MatchService.java
+++ TENdr/app/src/main/java/smap/gr15/appproject/tendr/services/MatchService.java
@@ -1,4 +1,15 @@
 package smap.gr15.appproject.tendr.services;
 
-public class MatchService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class MatchService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
