f87d167994cb518f0fe7c8bd70d041e1cd25f722
florent417 <flole@live.dk>
2020-04-16 15:53:49 +0200

    project added

---
+++ TENdr/app/src/test/java/smap/gr15/appproject/tendr/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package smap.gr15.appproject.tendr;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
