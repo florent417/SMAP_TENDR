bf91f38a5e809ec453ccfe85206fb6c6ebac96e4
florent417 <flole@live.dk>
2020-04-18 21:36:17 +0200

    added firebasestore

--- TENdr/app/src/main/java/smap/gr15/appproject/tendr/activities/ProfileActivity.java
+++ TENdr/app/src/main/java/smap/gr15/appproject/tendr/activities/ProfileActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import smap.gr15.appproject.tendr.R;
 
 public class ProfileActivity extends AppCompatActivity {
+    private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
