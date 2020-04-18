0af5053ceebfb909119d88f4779bd747a50f8397
florent417 <flole@live.dk>
2020-04-18 23:44:19 +0200

    b4 layout setup

--- TENdr/app/src/main/java/smap/gr15/appproject/tendr/activities/ProfileActivity.java
+++ TENdr/app/src/main/java/smap/gr15/appproject/tendr/activities/ProfileActivity.java
@@ -1,19 +1,54 @@
 package smap.gr15.appproject.tendr.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import smap.gr15.appproject.tendr.R;
+import smap.gr15.appproject.tendr.models.Profile;
+import smap.gr15.appproject.tendr.utils.Globals;
 
 public class ProfileActivity extends AppCompatActivity {
+    private static final String TAG = "ProfileActivity";
     private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        //testAddProfile();
+    }
+
+    private void testAddProfile(){
+        List<String> genderprefs = new ArrayList<>();
+
+        genderprefs.add("Man");
+
+        Profile testProf = new Profile("firstName", 22, "occupation", "city", "country", "gender", genderprefs, "email", "password");
+        firestore.collection(Globals.FIREBASE_Profiles_PATH).add(testProf).addOnSuccessListener(
+                new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "Added " + documentReference.getId());
+                    }
+                }
+        ).addOnFailureListener(
+                new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, e.getMessage());
+                    }
+                }
+        );
     }
 }
