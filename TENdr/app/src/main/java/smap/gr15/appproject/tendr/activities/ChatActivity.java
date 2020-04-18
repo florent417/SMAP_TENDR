26e4b508d41f7dcf1ecfde8f1219870997a6ab52
Berthelmaster <berthel0802@gmail.com>
2020-04-18 19:48:40 +0200

    removed garbage

--- TENdr/app/src/main/java/smap/gr15/appproject/tendr/activities/ChatActivity.java
+++ TENdr/app/src/main/java/smap/gr15/appproject/tendr/activities/ChatActivity.java
@@ -12,7 +12,5 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-
-        //asdasd
     }
 }
