4526d5059a4741b6b6740e74510000a9c459ed19
Berthelmaster <berthel0802@gmail.com>
2020-04-18 19:41:11 +0200

    qwdqwdqw

--- TENdr/app/src/main/java/smap/gr15/appproject/tendr/activities/ChatActivity.java
+++ TENdr/app/src/main/java/smap/gr15/appproject/tendr/activities/ChatActivity.java
@@ -12,5 +12,7 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        //asdasd
     }
 }
